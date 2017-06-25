57d01cc14627de36fa2b52d140d8b698f71044f1
Michael Ashton <mykl.ashton@gmail.com>
2017-06-25 10:00:43 -0400

    post epiphany

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// current working question at line 123
+
+
 var express = require('express');
 var fs = require('fs');
 var validator = require('express-validator');
@@ -19,25 +22,38 @@ app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+
+
+// v v  fileSync with err   v v
+// var rawWords = fs.readFileSync("/usr/share/dict/words", "utf-8", (err) => {
+//   if (err) throw err;
+//   console.log("file accessed succesfully");
+// });
+//
+// var words = rawWords.toLowerCase().split("\n");
+
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+// does this work, above? error says cannot read .split of undefined
+
+app.use(function (req, res, next) {
+
+  next()
+})
+
+
+
+
+
+
+
+
 app.use(session({       // setup sessions
   secret: 'cookiepartycookiepartycookieparty',
   resave: false,
   saveUninitialized: true
-}))
-
-// v v  variables   v v
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-
-
-does this work, above? error says cannot read .split of undefined
-
-
-
-// generate this v v when '/' is requested
-var randomWord = words[Math.floor(Math.random()*words.length)];
+}));
 
 app.get('/', function(req, res){
-    var session = req.session
     var markup;
 
   // if(session.views){ // if user has submitted data already
@@ -46,25 +62,31 @@ app.get('/', function(req, res){
   // else { // session START
 
     var randomWord = words[Math.floor(Math.random()*words.length)]; // generate random on start
-    var word = req.session.word;
-    var letterArray = word.split(""); // an array with each letter of the random word as an index
-    req.session.word = letterArray;
+    var sessionWord = randomWord.split(""); // an array with each letter of the random word as an index
+    // req.session.word = sessionWord; // req.session.word is now an array containing each letter of the random word
+    var letters = []; // create empty array of letters
+    for(i=0; i<sessionWord.length; i++){ // loop through sessionWord
+      letters.push({"letter": sessionWord[i], "correct": false});
+    };     // letters should now be a 2D array with objects for each letter
 
-    for (i=0; i < letterArray.length; i++){
-        markup += ` <span> _ </span> `
-    };
+    req.session.letters = letters;    // letters should now be stored in session
 
+
+    res.render('index', { letters: req.session.letters})
 
 });
 
-app.post('/', function(req, res){
 
+
+
+app.post('/', function(req, res){
 
 });
 
 app.listen(8080, function(){
-  console.log("Server Initialized");
+  console.log("GO FOR IT, G!");
 });
+
 
 
 
---
+++ pseudo.js
@@ -0,0 +1,108 @@
+// a session starts when user first requests '/'
+// user should see empty _ for each letter of the generated words
+// user should see an input field and be encouraged to guess a letter
+// user will still be viewing the "GET" at this point.
+/* once user clicks the submit button for the input form the user will be
+transfered to a POST that resubmits the HTML but now with the users guessed letter
+appears either in place of one of the "_" or if incorrect, in a list of guessed letters */
+// mustache requires the definition of a key-value pair. I could append an object to an array.
+//
+// if jsonfile.guesses.correct == "false"
+
+// will this work:
+
+var guesses = require('guesses.json')
+//or maybe needs:
+var guesses = guesses.readFile('./guesses.json', function(err, obj){
+  console.dir(obj);
+});
+//and guesses.json is something like:
+{ "guess": "m",
+  "correct": false
+}
+{ "guess": "t",
+  "correct": true
+}
+
+//so maybe if guesses.correct == true, add to markup....
+{{#guesses}}
+  {{#guesses.correct}}
+  <span>{{gueses.guess}}
+  {{/guesses.correct}}
+  {{^guesses.correct}}
+  <span>_<span>
+  {{/guesses.correct}}
+{{guesses.guess}}
+
+//will this work? If so you've nearly got the whole thing. Will test later.
+
+// this will only display "_" for the number of guesses.
+// maybe it's better to store the correct word in the json.
+// then when the user input matches guesses.letter, change guesses.correct to true.
+// but this stores a json file for every user interaction. Obviously not scalable, so maybe best just to store it in the mystery zone that is the session. So that might look like this:
+// -- ('index.mustache', { letters: req.session.guesses}) ... that's it, just store an object in the session. convert sessionWord[] to an object, maybe like this:
+var guesses = []
+for (i=0; i < sessionWord.length; i++){
+  guesses.push({"letter": sessionWord[i], "correct": false});
+} // this should append an object for each letter of the word. Time to test it:
+
+// -- Eyup, this creates an array containing an object for each letter with correct=false
+
+// req.session.guesses = guesses;
+// now guesses is stored in the session.
+// using templating in mustache, ('index', {letters: req.session.guesses})
+// then, from above but corrected:
+{{#letters}}
+  {{#letters.correct}}
+  <span>{{letters.letter}}
+  {{/letters.correct}}
+  {{^letters.correct}}
+  <span>_<span>
+  {{/letters.correct}}
+{{/letters}}
+
+// this probably wont display things in the correct order... will have to test later on.
+// if it doesn't store in the correct order, try rearranging the #/^/ to # ^ / /
+// -- in other words, nest the ^ inside #/ so maybe it wont upset the order
+
+
+// can I store letters and words in an object?
+// the object could have key:value pairs like:
+// --  {"letter": "o",
+// --   "guessed": false}
+// the question is how to a get the user's guess and use it change false to true
+// AND how do I use whether or not guessed equals false to tell whether or not to show _ or the letter
+
+//currently I have an array of letters. When user uses input to guess a letter
+// sessionWord = ['c', 'o', 'm', 'p', 'a', 's', 's', 'i', 'o', 'n']
+// -- var currentGuess = 's'
+// for (i=0: i < sessionWord.length; i++){
+//  if currentGuess =
+//};
+
+//maybe I can do this instead:
+// -- var currentGuess = [];
+// for (i=0: i < sessionWord.length; i++){
+//  currentGuess.push("_");
+// }
+// this logic works, but the array is hard to access in index.mustache.
+// definitely better to have currentGuess and
+
+so store guessed letters in an object with guesses {"letter": "s", "correct": true}
+for (i=0: i < sessionWord.length; i++){
+  if (req.body.guess == "sessionWord[i]"){
+    var obj = req.body.guess;
+    jsonOfGuesses.writeFile(file, obj, {flag: 'a'}, function (err) {
+        console.log(err);
+      });
+    }
+    else {
+      ?
+    }
+};
+
+
+
+
+
+//
